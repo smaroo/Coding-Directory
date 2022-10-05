ca390a97b7f4610fc2575572cf955ca5bfea96e9
smaroo <6109215+smaroo@users.noreply.github.com>
2022-10-05 10:29:39 -0400

    New commits

--- alert.js
+++ alert.js
@@ -1,1 +1,11 @@
+// Enables ECMAScript 5 (ES5) features
+
+"use strict"
+
+/* This is a javascript file 
+It is a simple browser pop-up alert with a message
+*/
+
 alert( "Hello world! Myah!!!" );
+
+// ctrl+slash is a hotkey to create comments
