02bf1a5b606c5cc7875cbe5d05f6457c2f11c11f
smaroo <6109215+smaroo@users.noreply.github.com>
2022-10-05 10:30:09 -0400

    New file

---
+++ Variables.js
@@ -0,0 +1,44 @@
+"use strict";
+
+//variable names can only contain letters, digits, or symbols $ and _ they are also case-sensitive
+
+//An extra variable is good, not evil. saves time on debugging
+
+//const are for unchangeable variables (e.g., my birthday) capital-named constants are only for hard-coded values
+
+const myBirthday = '09.20.1993';
+
+//creates the variable
+let message1;
+
+//stores the string 'Hello' int he variable named 'message'
+message1 = 'Hello'; 
+
+//to be concise, it can be one line
+
+let message2 = 'Hola';
+
+//Can also declare multiple variables in one line
+let message3 = 'Aloha', great = 'You', notgreat = 'Me';
+
+//Best practice is 1 variable per line
+
+let team1 = 'Blue';
+let team2 = 'Red';
+let game = 'Halo';
+
+//test
+
+let name;
+let admin;
+
+name = "John";
+
+admin = name;
+
+alert("admin");
+
+//test 2
+
+let ourPlanetName = "Earth";
+let currentUser = "John";
